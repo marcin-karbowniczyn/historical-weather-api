d0e0652b1bd3e02522b6538ef9d9163cafc9e331
Marcin <marcin.karbowniczyn@gmail.com>
2023-12-01 19:59:53 +0100

    Add URL endpoint ffor yearly data, and all data

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def home():
 # If I return a dictionary, Flask will automatically convert it to JSON, therefore I don't need to use jsonify().
 # Although it's good to be aware of how does it work behin the scenes.
 @app.route('/api/v1/<station>/<date>')
-def about(station, date):
+def station_on_day_data(station, date):
     try:
         # Funckja zfill() bierze string i dopełnia go zerami na podaną długość -> station = 10 -> station.zfill(6) = '000010'
         filename = f"data_small/TG_STAID{station.zfill(6)}.txt"
@@ -32,6 +32,25 @@ def about(station, date):
     except TypeError:
         return {"error": "TypeError",
                 "description": "Wrong date or index format. Make sure index is an integer, and a date is in format YYYY-MM-DD"}
+
+
+# All results from one station
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = f"data_small/TG_STAID{station.zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    # Opcja records tworzy list of dicts (konwertuje dataframe w list of dicts), gdzie każdy dict to jeden row/rekord
+    return df.to_dict(orient="records")
+
+
+# All results from one staton in a specific year
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = f"data_small/TG_STAID{station.zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 # debug=True will allow us to see errors on the webpage.
