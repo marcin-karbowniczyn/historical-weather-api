c1bfed972fede7f260ae55508f5875251e459bb5
Marcin <marcin.karbowniczyn@gmail.com>
2023-12-01 12:25:26 +0100

    Return temperature for any station and date. Add data files.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,12 +11,19 @@ def home():
 
 
 # <> -> special Flask syntax, denotes that users can enter values for these 2 parameters
+# If I return a dictionary, Flask will automatically convert it to JSON, therefore I don't need to use jsonify().
+# Although it's good to be aware of how does it work behin the scenes.
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
-    temperature = 23
-    return {"station": station, "date": date, "temperature": temperature}
+    # Funckja zfill() bierze string i dopełnia go zerami na podaną długość -> station = 10 -> station.zfill(6) = '000010'
+    filename = f"data_small/TG_STAID{station.zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+
+    # Nie muszę wpisywać jsonify(), Flask zrobi to automatycznie jak zobaczy obiekt którzy jest JSON serializable, ale wolałem sobie tak wpisać, żeby pamiętać
+    return jsonify({"station": station, "date": date, "temperature": temperature})
 
 
 # debug=True will allow us to see errors on the webpage.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)  # Port is set default to 5000, but we can explecitly specify it to something else, like 5001.
